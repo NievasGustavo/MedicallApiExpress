041ddb4aa94bdf20f10acd8a7542d36eaffac5e9
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-05-08 18:34:48 -0400

    add 🤓☝🏽

--- src/models/entities/users.models.js
+++ src/models/entities/users.models.js
@@ -3,48 +3,54 @@ import { sequelize } from "../../database/database.js";
 
 const { UUID, STRING, DATEONLY, INTEGER } = DataTypes;
 
-export const Users = sequelize.define("Users", {
-	ID: {
-		type: UUID,
-		primaryKey: true,
-		defaultValue: DataTypes.UUIDV4,
+export const Users = sequelize.define(
+	"users",
+	{
+		id: {
+			type: UUID,
+			primaryKey: true,
+			defaultValue: DataTypes.UUIDV4,
+		},
+		first_name: {
+			type: STRING,
+			allowNull: false,
+		},
+		last_name: {
+			type: STRING,
+			allowNull: false,
+		},
+		dni: {
+			type: STRING(20),
+			allowNull: false,
+		},
+		birthdate: {
+			type: DATEONLY,
+		},
+		country_id: {
+			type: INTEGER,
+			allowNull: false,
+		},
+		phone: {
+			type: STRING(20),
+		},
+		gender: {
+			type: STRING(10),
+		},
+		email: {
+			type: STRING,
+			allowNull: false,
+			unique: true,
+		},
+		password: {
+			type: STRING,
+			allowNull: false,
+		},
+		rol_id: {
+			type: INTEGER,
+			allowNull: false,
+		},
 	},
-	first_name: {
-		type: STRING,
-		allowNull: false,
+	{
+		timestamps: false,
 	},
-	last_name: {
-		type: STRING,
-		allowNull: false,
-	},
-	dni: {
-		type: STRING(20),
-		allowNull: false,
-	},
-	birthdate: {
-		type: DATEONLY,
-	},
-	country_id: {
-		type: INTEGER,
-		allowNull: false,
-	},
-	phone: {
-		type: STRING(20),
-	},
-	gender: {
-		type: STRING(10),
-	},
-	email: {
-		type: STRING,
-		allowNull: false,
-		unique: true,
-	},
-	password: {
-		type: STRING,
-		allowNull: false,
-	},
-	rol_id: {
-		type: INTEGER,
-		allowNull: false,
-	},
-});
+);
--- src/routes/accounts/patients.routes.js
+++ src/routes/accounts/patients.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-
+import * as controller from "../../controllers/accounts/patients.controllers.js";
 const router = Router();
 
-// router.get("/patients/", controller.getPatients);
+router.get("/patients/", controller.getPatients);
 // router.get("/patients/:id", controller.getPatient);
 // router.post("/patients/", controller.createPatient);
 // router.put("/patients/:id", controller.updatePatient);
